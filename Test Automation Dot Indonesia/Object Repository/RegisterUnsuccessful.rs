2fab59e8286577c26c0ce66c740cb21ec1705b3b
unknown <PugarHuda>
2022-03-18 01:06:25 +0700

    Test Online

---
+++ Test Automation Dot Indonesia/Object Repository/RegisterUnsuccessful.rs
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>RegisterUnsuccessful</name>
+   <tag></tag>
+   <elementGuidId>df643b0f-d03b-43b4-a41d-006564337945</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <katalonVersion>8.2.5</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>https://reqres.in/api/register</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Test Automation Dot Indonesia/Test Suites/Validation/UserValidation.ts
@@ -0,0 +1,104 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>UserValidation</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>627192db-dc73-4d05-b86a-8a2fe71bdf8f</testSuiteGuid>
+   <testCaseLink>
+      <guid>8108f99f-21a7-4c10-ae23-6adb7c1ac09b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseDot/ValidateCreate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>45c65984-3a83-4c97-a6a4-68bf1dfa5377</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseDot/ValidateDelayedResponse</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1a6dfe64-04df-4521-9a6f-da8cad646018</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseDot/ValidateDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>34aa4290-75d3-4504-acbb-f767f7ec7ba4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseDot/ValidateListResource</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d369b3fc-9257-4681-aa9f-67b3379282b9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseDot/ValidateListUser</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f177626f-fae9-4ab3-ba6d-13dca6f0e558</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseDot/ValidateLoginSuccessfully</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b882d957-dd04-46a3-a1f5-f12960b8a29b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseDot/ValidateLoginUnsuccessful</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d6035cf4-7a13-46a6-b30f-575eee9e679d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseDot/ValidateRegisterSuccessfully</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>60a14341-fb81-4f6c-bbf4-7b0f68c02ae8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseDot/ValidateRegisterUnsuccessful</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>84adf37d-a76b-4a2a-87a8-8781fc48545a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseDot/ValidateSingleResource</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0f5bd8d9-487b-4b90-ad10-ba278c741513</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseDot/ValidateSingleResourceNotFound</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>038b1616-dd0a-4e3f-8032-af13da3f50ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseDot/ValidateSingleUser</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d09a30f6-e58b-41a3-9bd6-a9c4726cce94</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseDot/ValidateSingleUserNotFound</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d83f872c-2050-4a1b-9ea4-d632ec4830f9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseDot/ValidateUpdate</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9e2a177d-ad33-40a3-97b0-3305d6c9a5f8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseDot/ValidateUpdate2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
